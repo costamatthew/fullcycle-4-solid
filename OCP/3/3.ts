2d7c323ef2c4a699cf77ff95817126dfc10ac835
mateus <mateusdsilvacosta@outlook.com>
2026-07-20 23:00:55 -0300

    Repositorio de Strategies

---
+++ OCP/3/3.ts
@@ -0,0 +1,10 @@
+class ValidatorExample {
+    validateExample(field: string, value: string): boolean {
+        if (field === "email") {
+            return /\S+@\S+\.\S+/.test(value);
+        } else if (field === "phone") {
+            return /^\d{10}$/.test(value);
+        }
+        return false;
+    }
+}
